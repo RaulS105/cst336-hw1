cdfc26f65412e6c8f6b0b863b6eaf1a9c261bc89
Raul Sanchez <10559683+RaulS105@users.noreply.github.com>
2020-11-03 11:41:56 -0800

    Update index.php

--- index.php
+++ index.php
@@ -30,14 +30,12 @@
             <a href="tutorial.html"> Tips and Tricks </a>
         </nav>
 
-        <div class="a"> <img src="img/java.jpg"> 
+        <div class="a"> <img src="img/java.jpg" alt="java image!!"> 
                         Java is a class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible, 
                         explore this site to gain a better insight on what makes this programming language work and also finding the history of how this 
                         language camet to be!
                     	This site should help you gain a better insight on what Java is and how to become a better programming with some simple tips!</div>
         <a href="https://www.edureka.co/blog/what-is-java/">What is Java?</a>
-
-        </main>
 
     </body> 
 
